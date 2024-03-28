368e628daa7bead465daec437aa70a709a41749c
koraphat <koraphat.lum@gmail.com>
2024-03-28 14:18:18 +0700

    add logout sys

--- controllers/auth.js
+++ controllers/auth.js
@@ -59,6 +59,22 @@ exports.login = async (req,res,next) =>{
     sendTokenResponse(user , 200 , res);
 };
 
+//@desc     Logout user
+//@route    GET api/v1/auth/logout
+//@access   Private
+
+exports.logout = async (req,res,next) =>{
+    res.cookie('token' , 'none' ,{
+        expires: new Date(Date.now() + 10*1000),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success:true,
+        data:{}
+    })
+}
+
 // get token from model, create cookie and send response
 
 const sendTokenResponse = (user , statusCode , res) =>{
