f28c9979a0a5485dd402b22050538a353318cb2a
koraphat <koraphat.lum@gmail.com>
2024-04-09 20:43:44 +0700

    some auto formatter

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -1,218 +1,231 @@
-const Appointment = require('../models/Appointment');
-const Hospital = require('../models/Hospital');
-
+const Appointment = require("../models/Appointment");
+const Hospital = require("../models/Hospital");
 
 //@desc     Get all appointments
 //@route    GET api/v1/appointments
 //@access   Public
 
-exports.getAppointments = async (req , res , next) =>{
-    let query;
+exports.getAppointments = async (req, res, next) => {
+  let query;
 
-    // General User can see only their appointments!
-    if (req.user.role !== 'admin'){
-        console.log(req.user.name);
-        query = Appointment.find({user:req.user.id}).populate({
-            path: 'hospital',
-            select: 'name province tel'
-        });
-        // console.log(query)
+  // General User can see only their appointments!
+  if (req.user.role !== "admin") {
+    console.log(req.user.name);
+    query = Appointment.find({ user: req.user.id }).populate({
+      path: "hospital",
+      select: "name province tel",
+    });
+    // console.log(query)
+  } else {
+    // Admin can see all
+    if (req.params.hospitalId) {
+      let hosId = req.params.hospitalId;
+
+      console.log(hosId);
+
+      query = Appointment.find({ hospital: hosId }).populate({
+        path: "hospital",
+        select: "name province tel",
+      });
+    } else {
+      query = Appointment.find().populate({
+        path: "hospital",
+        select: "name province tel",
+      });
     }
-    else{ // Admin can see all
-        if(req.params.hospitalId){
-            let hosId = req.params.hospitalId;
+  }
 
-            console.log(hosId);
+  try {
+    const appointmentsData = await query;
 
-            query = Appointment.find({hospital:hosId}).populate({
-                path: 'hospital',
-                select: 'name province tel'
-            });
-        }
-        else{
-            query = Appointment.find().populate({
-                path: 'hospital',
-                select: 'name province tel'
-            });
-        }
-    }
-
-    try {
-        const appointmentsData = await query;
-        
-        res.status(200).json({
-            success: true,
-            count: appointmentsData.length,
-            data : appointmentsData
-        });
-
-    }
-    catch(error){
-        
+    res.status(200).json({
+      success: true,
+      count: appointmentsData.length,
+      data: appointmentsData,
+    });
+  } catch (error) {
     console.log(error);
     return res.status(500).json({
-        success: false,
-        message: "Cannot find appointment"
+      success: false,
+      message: "Cannot find appointment",
     });
-    
-    }  
+  }
 };
 
 //@desc     Get single appointment
 //@route    GET api/v1/appointments/:id
 //@access   Public
 
-exports.getAppointment = async (req , res , next)=> {
-    try{
-        let hid = req.params.id;
-        const appointment = await Appointment.findById(hid).populate({
-            path : 'hospital',
-            select: 'name description tel'
-        });
+exports.getAppointment = async (req, res, next) => {
+  try {
+    let hid = req.params.id;
+    const appointment = await Appointment.findById(hid).populate({
+      path: "hospital",
+      select: "name description tel",
+    });
 
-        if(!appointment){
-            return res.status(404).json({success : false , message: `No appointment with the id of ${hid}`});
-        }
-
-        res.status(200).json({
-            success : true,
-            data: appointment
-        });
-    }catch(error){
-        console.log(error);
-        return res.status(500).json({success:false , massage : "Cannot find Appointment"});
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        message: `No appointment with the id of ${hid}`,
+      });
     }
-}
+
+    res.status(200).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, massage: "Cannot find Appointment" });
+  }
+};
 
 //@desc     Add a appointments
 //@route    POST api/v1/hospitals/:hospitalId/appointment
 //@access   Private
 
-exports.addAppointment = async (req , res , next)=> {
-    try{
-        const hid = req.params.hospitalId;
-        
-        req.body.hospital = hid;
+exports.addAppointment = async (req, res, next) => {
+  try {
+    const hid = req.params.hospitalId;
 
-        // Check if hospital exist
-        const hospital = await Hospital.findById(hid);
+    req.body.hospital = hid;
 
-        if(!hospital){
-            return res.status(404).json({success:false ,massage : `No hospital with the id of ${hid}`})
-        }
-        //add user Id to req.body
-        req.body.user = req.user.id;
-        
-        const existedAppointments = await Appointment.find({ user : req.user.id});
+    // Check if hospital exist
+    const hospital = await Hospital.findById(hid);
 
-        // console.log(existedAppointments.length);
-        // If user is not an admin, they can only create 3 appointment
-        if(existedAppointments.length >= 3 && req.user.role !== "admin"){
-            return res.status(400).json({success:false ,massage : `The user with id ${req.user.id} has already made 3 appointments`})
-        }
-
-        const appointment = await Appointment.create(req.body);
-
-        res.status(200).json({
-            success:true,
-            data: appointment
-        });
-
-
-    }catch(error){
-        console.log(error);
-
-        res.status(500).json({
-            success:false,
-            massage : "Cannot create Appointment"
-        });
+    if (!hospital) {
+      return res
+        .status(404)
+        .json({ success: false, massage: `No hospital with the id of ${hid}` });
     }
-}
+    //add user Id to req.body
+    req.body.user = req.user.id;
+
+    const existedAppointments = await Appointment.find({ user: req.user.id });
+
+    // console.log(existedAppointments.length);
+    // If user is not an admin, they can only create 3 appointment
+    if (existedAppointments.length >= 3 && req.user.role !== "admin") {
+      return res.status(400).json({
+        success: false,
+        massage: `The user with id ${req.user.id} has already made 3 appointments`,
+      });
+    }
+
+    const appointment = await Appointment.create(req.body);
+
+    res.status(200).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      massage: "Cannot create Appointment",
+    });
+  }
+};
 
 //@desc     Update a appointments
 //@route    PUT api/v1/hospitals/:hospitalId/appointment
 //@access   Private
 
-exports.updateAppointment = async (req , res , next)=> {
-    try{
-        const apptid = req.params.id;
-        const user = req.user;
-        
-        //check if appointment existed
+exports.updateAppointment = async (req, res, next) => {
+  try {
+    const apptid = req.params.id;
+    const user = req.user;
 
-        let appointment = await Appointment.findById(apptid);
+    //check if appointment existed
 
-        if(!appointment){
-            return res.status(404).json({success:false ,massage : `No appointment with the id of ${apptid}`})
-        }
-        //make sure if user is the appointment owner
-        if(appointment.user.toString() !== user.id && user.role !== "admin" ){
-            return res.status(404).json({success:false ,massage : `User ${user.id} is not authorized to update this appointment`})
-        }
+    let appointment = await Appointment.findById(apptid);
 
-        appointment = await Appointment.findByIdAndUpdate(apptid , req.body , { new:true, runValidators:true});
-
-        res.status(200).json({
-            success:true,
-            data: appointment
-        });
-
-    }catch(error){
-        console.log(error);
-
-        res.status(500).json({
-            success:false,
-            massage : "Cannot update Appointment"
-        });
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        massage: `No appointment with the id of ${apptid}`,
+      });
     }
-}
+    //make sure if user is the appointment owner
+    if (appointment.user.toString() !== user.id && user.role !== "admin") {
+      return res.status(404).json({
+        success: false,
+        massage: `User ${user.id} is not authorized to update this appointment`,
+      });
+    }
+
+    appointment = await Appointment.findByIdAndUpdate(apptid, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: appointment,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      massage: "Cannot update Appointment",
+    });
+  }
+};
 
 //@desc     Delete a appointments
 //@route    DELETE api/v1/hospitals/:hospitalId/appointment
 //@access   Private
 
-exports.deleteAppointment = async (req , res , next)=> {
-        try{
-            const apptid = req.params.id;
-            const user = req.user
-        
-            const appointment = await Appointment.findById(apptid);
+exports.deleteAppointment = async (req, res, next) => {
+  try {
+    const apptid = req.params.id;
+    const user = req.user;
 
-            if(!appointment){
-                return res.status(404).json({success:false ,massage : `No appointment with the id of ${apptid}`})
-            }
+    const appointment = await Appointment.findById(apptid);
 
-            //make sure if user is the appointment owner
-            if(appointment.user.toString() !== user.id && user.role !== "admin" ){
-                return res.status(404).json({success:false ,massage : `User ${user.id} is not authorized to delete this bootcamp`})
-            }
-            await appointment.deleteOne();
+    if (!appointment) {
+      return res.status(404).json({
+        success: false,
+        massage: `No appointment with the id of ${apptid}`,
+      });
+    }
 
-            res.status(200).json({
-                success:true,
-                data:{}
-            });
+    //make sure if user is the appointment owner
+    if (appointment.user.toString() !== user.id && user.role !== "admin") {
+      return res.status(404).json({
+        success: false,
+        massage: `User ${user.id} is not authorized to delete this bootcamp`,
+      });
+    }
+    await appointment.deleteOne();
 
-        }catch(error){
-            console.log(error);
+    res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    console.log(error);
 
-            res.status(500).json({
-                success:false,
-                massage : "Cannot delete Appointment"
-            });
-        }
-
-
-}
+    res.status(500).json({
+      success: false,
+      massage: "Cannot delete Appointment",
+    });
+  }
+};
 //==========================================================
 
 //@desc     Create a appointments for testing purpose only
 //@route    Post api/v1/appointments
 //@access   Public
 
-exports.createAppointment = async (req , res , next) => {
-    // console.log(req.body);
-    // res.status(200).json({success: true , msg:`Create new hospitals`});
-    const appointment = await Appointment.create(req.body);
-    res.status(201).json({success:true  , data: appointment });
+exports.createAppointment = async (req, res, next) => {
+  // console.log(req.body);
+  // res.status(200).json({success: true , msg:`Create new hospitals`});
+  const appointment = await Appointment.create(req.body);
+  res.status(201).json({ success: true, data: appointment });
 };
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,128 +1,130 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 //@desc     Register user
 //@route    POST api/v1/auth/register
 //@access   Public
 
-exports.register = async (req , res , next) => {
+exports.register = async (req, res, next) => {
+  // res.status(200).json({success:true});
+  try {
+    const { name, email, password, role } = req.body;
 
-    // res.status(200).json({success:true});
-    try{
-        const {name , email , password , role} = req.body;
+    //create user to the database
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+    });
 
-        //create user to the database
-        const user = await User.create({
-            name , 
-            email, 
-            password,
-            role
-        });
-
-        sendTokenResponse(user , 200 , res);
-        // res.status(200).json({success:true , data: req.body});
-    }
-    catch(err){
-        res.status(400).json({success:false});
-        console.log(err.stack);
-    }
+    sendTokenResponse(user, 200, res);
+    // res.status(200).json({success:true , data: req.body});
+  } catch (err) {
+    res.status(400).json({ success: false });
+    console.log(err.stack);
+  }
 };
 
 //@desc     Login user
 //@route    POST api/v1/auth/login
 //@access   Public
 
-exports.login = async (req,res,next) =>{
-
-    try{
-        const {email , password} = req.body;
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
     // Validate email & password
-    if (!email || ! password){
-        return res.status(400).json({success : false , msg: 'Please provide an email and password'});
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Please provide an email and password" });
     }
 
-    const user = await User.findOne({email}).select('+password');
+    const user = await User.findOne({ email }).select("+password");
 
-    if(!user){
-        // return res.status(400).json({success : false , msg: 'Invalid credentials (user not found)'});
-        return res.status(400).json({success : false , msg: 'Invalid credentials'});
+    if (!user) {
+      // return res.status(400).json({success : false , msg: 'Invalid credentials (user not found)'});
+      return res
+        .status(400)
+        .json({ success: false, msg: "Invalid credentials" });
     }
 
     // Check if password matches
 
     const isMatch = await user.matchPassword(password);
 
-    if(!isMatch){
-        // return res.status(401).json({success : false , msg: 'Invalid credentials (password incorrect)'});
-        return res.status(401).json({success : false , msg: 'Invalid credentials'});
+    if (!isMatch) {
+      // return res.status(401).json({success : false , msg: 'Invalid credentials (password incorrect)'});
+      return res
+        .status(401)
+        .json({ success: false, msg: "Invalid credentials" });
     }
 
-    sendTokenResponse(user , 200 , res);
-    }catch(err){
-        return res.status(401).json({
-        success:false,
-        msg:'Cannot convert email or password to string'
-        });
-}
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      msg: "Cannot convert email or password to string",
+    });
+  }
 };
 
 //@desc     Logout user
 //@route    GET api/v1/auth/logout
 //@access   Private
 
-exports.logout = async (req,res,next) =>{
-    res.cookie('token' , 'none' ,{
-        expires: new Date(Date.now() + 10*1000),
-        httpOnly: true
-    });
+exports.logout = async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
 
-    res.status(200).json({
-        success:true,
-        data:{}
-    })
-}
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+};
 
 // get token from model, create cookie and send response
 
-const sendTokenResponse = (user , statusCode , res) =>{
-    // Create token
-    const token = user.getSignedJwtToken(); 
-    
-    const options = {
-        expires : new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-        httpOnly : true
-    };
+const sendTokenResponse = (user, statusCode, res) => {
+  // Create token
+  const token = user.getSignedJwtToken();
 
-    if(process.env.NODE_ENV === "production"){
-        options.secure = true;
-    }
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
 
-    res.status(statusCode).cookie('token' , token , options).json({
+  if (process.env.NODE_ENV === "production") {
+    options.secure = true;
+  }
+
+  res.status(statusCode).cookie("token", token, options).json({
     // res.status(statusCode).json({
-        success: true,
-        
-        // add for frontend
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        // end for frontend
-        token
-    })
-    // console.log('from backend',user._id , user.name ,user.email );
-    // print('from backend',user._id , user.name ,user.email )
-}
+    success: true,
+
+    // add for frontend
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    // end for frontend
+    token,
+  });
+  // console.log('from backend',user._id , user.name ,user.email );
+  // print('from backend',user._id , user.name ,user.email )
+};
 
 //@desc     Get current Logged in user
 //@route    Get api/v1/auth/me
 //@access   Private
 
-exports.getMe = async(req , res , next)=>{
-
-    const user = await User.findById(req.user.id);
-    res.status(200).json(
-        {
-            success: true,
-            data: user
-        });
-
+exports.getMe = async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 };
